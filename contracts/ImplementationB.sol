9f77a0eb392546a7d6cae6e2de1cf395836ea49b
Sayrarh <ajaotosinserah@gmail.com>
2022-09-15 21:57:08 +0100

    token upgrade

---
+++ contracts/ImplementationB.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "./Proxiable.sol";
+
+contract ImplementationA is Proxiable, ERC20("Classwork", "CLW")  {
+    address public owner;
+
+    
+    function initialize() public {
+        require(owner == address(0), "Already initalized"); //checks to see if the owner has triggered the function before
+        owner = msg.sender;
+        _mint(address(this), 20000e18 );
+    }
+
+    function encode() public pure returns(bytes memory){
+        return abi.encodeWithSignature("initialize()");
+    }
+
+    function mint(address to, uint amount) external {
+        _mint(to, amount);
+    }
+
+    function transferTokenOut(address to, uint amount)external onlyOwner  {
+        uint tokenBalance = balanceOf(address(this));
+        require (tokenBalance >= amount , "You can't send more than balance");
+        _transfer(address(this), to , amount );
+    }
+
+    function mint(address to) public{
+        uint burnable = (totalSupply() * 1) / (totalSupply() * 10000);
+        _burn(to, burnable);
+    }
+
+    function updateCode(address newCode) onlyOwner public {
+        updateCodeAddress(newCode);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner is allowed to perform this action");
+        _;
+    }
+}
